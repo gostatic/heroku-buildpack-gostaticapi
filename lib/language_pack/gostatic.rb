6695f555709642024d65590f99479275aba9003c
Marc Qualie <marc@marcqualie.com>
2015-06-04 23:43:09 +0100

    Install Jekyll after all other gems

--- lib/language_pack/gostatic.rb
+++ lib/language_pack/gostatic.rb
@@ -13,9 +13,9 @@ class LanguagePack::Gostatic < LanguagePack::Rails41
 
   def compile
     instrument "gostatic.compile" do
+      super
       puts "Installing Jekyll 2.5.3"
       pipe "/app/bin/gem install jekyll -v 2.5.3", out: "2>&1", user_env: true
-      super
     end
   end
 
