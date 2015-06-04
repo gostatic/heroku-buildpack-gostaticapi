70e91f7ab5e1d10c516e9ffabda1ffb2ebe0aac6
Marc Qualie <marc@marcqualie.com>
2015-06-04 23:11:11 +0100

    Downgrade to Rails 4.1 and force use of Gostatic language pack

--- lib/language_pack/gostatic.rb
+++ lib/language_pack/gostatic.rb
@@ -1,17 +1,10 @@
 require "language_pack"
 require "language_pack/rails42"
 
-class LanguagePack::Gostatic < LanguagePack::Rails42
-  # detects if this is a Rails 4.2 app
-  # @return [Boolean] true if it's a Rails 4.2 app
+class LanguagePack::Gostatic < LanguagePack::Rails41
+
   def self.use?
-    instrument "rails42.use" do
-      rails_version = bundler.gem_version('railties')
-      return false unless rails_version
-      is_rails42 = rails_version >= Gem::Version.new('4.2.0') &&
-                   rails_version <  Gem::Version.new('5.0.0')
-      return is_rails42
-    end
+    true
   end
 
   def compile
