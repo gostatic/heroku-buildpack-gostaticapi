3f1a7e3c8f6401fc74d3091200807be385f9b020
Marc Qualie <marc@marcqualie.com>
2015-06-05 20:00:28 +0100

    Remove bundle cache code entirely from custom language pack

--- lib/language_pack/gostatic.rb
+++ lib/language_pack/gostatic.rb
@@ -22,20 +22,7 @@ class LanguagePack::Gostatic < LanguagePack::Rails41
     jekyll_version = "2.4.0"
     puts "Installing Jekyll #{jekyll_version}"
     pipe "/app/bin/gem install jekyll -v #{jekyll_version}", out: "2>&1", user_env: true
-    # clean_bundler_cache # Don't call this as it removes Jekyll
     @bundler_cache.store
-  end
-
-  def clean_bundler_cache
-    puts "Cleaning up the bundler cache."
-    instrument "ruby.bundle_clean" do
-      # Only show bundle clean output when not using default cache
-      if load_default_cache?
-        run("bundle clean > /dev/null", user_env: true)
-      else
-        pipe("bundle clean", out: "2> /dev/null", user_env: true)
-      end
-    end
   end
 
   def default_config_vars
